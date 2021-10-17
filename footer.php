e51c80da322fcacd587e24f77f9b076eb0ac5de3
SeoBoSeok <ggy7bbo@gmail.com>
2021-10-18 08:00:42 +0900

    hot fix

--- footer.php
+++ footer.php
@@ -15,6 +15,259 @@
 		<?php
 		liquid_action( 'before_footer' );
 		liquid_action( 'footer' );
+		?>
+		<footer class="main-footer site-footer footer" id="footer" itemscope="itemscope" itemtype="http://schema.org/WPFooter">
+			<div data-elementor-type="wp-post" data-elementor-id="547" class="elementor elementor-547" data-elementor-settings="[]">
+							<div class="elementor-section-wrap">
+							<section class="elementor-section elementor-top-section elementor-element elementor-element-518ab29 elementor-section-full_width elementor-section-height-default elementor-section-height-default" data-id="518ab29" data-element_type="section">
+						<div class="elementor-container elementor-column-gap-no">
+					<div class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-fc25ca6" data-id="fc25ca6" data-element_type="column">
+			<div class="elementor-widget-wrap elementor-element-populated">
+								<div class="elementor-element elementor-element-50b85cd elementor-widget elementor-widget-ld_fancy_image" data-id="50b85cd" data-element_type="widget" data-widget_type="ld_fancy_image.default">
+				<div class="elementor-widget-container">
+			
+			<div id="" class="lqd-imggrp-single">
+				
+				<div class="lqd-imggrp-img-container">
+					
+							
+										<figure><img src="https://architecturehub.liquid-themes.com/elementor/wp-content/uploads/2021/08/img-1@2x-11.jpg" title="" alt=""></figure>
+																				
+				</div>
+			</div>
+
+				</div>
+				</div>
+					</div>
+		</div>
+							</div>
+		</section>
+				<section class="elementor-section elementor-top-section elementor-element elementor-element-4412e71 elementor-section-full_width elementor-section-height-default elementor-section-height-default" data-id="4412e71" data-element_type="section" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+						<div class="elementor-container elementor-column-gap-no">
+					<div class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-f2ef009" data-id="f2ef009" data-element_type="column" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+			<div class="elementor-widget-wrap elementor-element-populated">
+								<section data-section-luminosity="dark" class="elementor-section elementor-inner-section elementor-element elementor-element-0917a70 elementor-section-full_width elementor-section-height-default elementor-section-height-default" data-id="0917a70" data-element_type="section" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+						<div class="elementor-container elementor-column-gap-default">
+					<div class="elementor-column elementor-col-100 elementor-inner-column elementor-element elementor-element-df0add6" data-id="df0add6" data-element_type="column">
+			<div class="elementor-widget-wrap elementor-element-populated">
+								<div class="elementor-element elementor-element-2619476 elementor-widget elementor-widget-image" data-id="2619476" data-element_type="widget" data-widget_type="image.default">
+				<div class="elementor-widget-container">
+															<img src="https://architecturehub.liquid-themes.com/elementor/wp-content/uploads/2021/08/logo-light1-1.svg" title="" alt="">															</div>
+				</div>
+				<div class="elementor-element elementor-element-bfc26c8 elementor-widget__width-initial elementor-widget-tablet__width-inherit elementor-widget elementor-widget-hub_fancy_heading" data-id="bfc26c8" data-element_type="widget" data-widget_type="hub_fancy_heading.default">
+				<div class="elementor-widget-container">
+			
+		<div id="ld-fancy-heading-616ca60adc7b1" class="ld-fancy-heading">
+			<h2 class="ld-fh-element elementor-heading-title lqd-highlight-underline lqd-highlight-grow-left h4"> Let's Build Something Great Together</h2>		</div>
+
+				</div>
+				</div>
+					</div>
+		</div>
+							</div>
+		</section>
+				<section data-section-luminosity="dark" class="elementor-section elementor-inner-section elementor-element elementor-element-0fbcc16 elementor-section-full_width elementor-section-height-default elementor-section-height-default" data-id="0fbcc16" data-element_type="section" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+						<div class="elementor-container elementor-column-gap-default">
+					<div class="elementor-column elementor-col-100 elementor-inner-column elementor-element elementor-element-3772b09" data-id="3772b09" data-element_type="column">
+			<div class="elementor-widget-wrap elementor-element-populated">
+								<div class="elementor-element elementor-element-2cbc02f elementor-widget elementor-widget-hub_fancy_heading" data-id="2cbc02f" data-element_type="widget" data-widget_type="hub_fancy_heading.default">
+				<div class="elementor-widget-container">
+			
+		<div id="ld-fancy-heading-616ca60adf3bb" class="ld-fancy-heading">
+			<p class="ld-fh-element lqd-highlight-underline lqd-highlight-grow-left p"> London</p>		</div>
+
+				</div>
+				</div>
+					</div>
+		</div>
+				<div class="elementor-column elementor-col-25 elementor-inner-column elementor-element elementor-element-d51cf02" data-id="d51cf02" data-element_type="column">
+			<div class="elementor-widget-wrap elementor-element-populated">
+								<div class="elementor-element elementor-element-723f0e0 elementor-widget elementor-widget-hub_fancy_heading" data-id="723f0e0" data-element_type="widget" data-widget_type="hub_fancy_heading.default">
+				<div class="elementor-widget-container">
+			
+		<div id="ld-fancy-heading-616ca60ae0fab" class="ld-fancy-heading">
+			<p class="ld-fh-element lqd-highlight-underline lqd-highlight-grow-left p"> 21 Valentin Paterson Road 24, London</p>		</div>
+
+				</div>
+				</div>
+					</div>
+		</div>
+				<div class="elementor-column elementor-col-25 elementor-inner-column elementor-element elementor-element-28ae1a5" data-id="28ae1a5" data-element_type="column">
+			<div class="elementor-widget-wrap elementor-element-populated">
+								<div class="elementor-element elementor-element-ff8e608 elementor-widget elementor-widget-hub_fancy_heading" data-id="ff8e608" data-element_type="widget" data-widget_type="hub_fancy_heading.default">
+				<div class="elementor-widget-container">
+			
+		<div id="ld-fancy-heading-616ca60ae2be1" class="ld-fancy-heading">
+			<p class="ld-fh-element lqd-highlight-underline lqd-highlight-grow-left p"> + 124 875 89 90 info@liquid-themes.com</p>		</div>
+
+				</div>
+				</div>
+					</div>
+		</div>
+				<div class="elementor-column elementor-col-25 elementor-inner-column elementor-element elementor-element-32cd804" data-id="32cd804" data-element_type="column">
+			<div class="elementor-widget-wrap elementor-element-populated">
+								<div class="elementor-element elementor-element-a645df9 elementor-widget elementor-widget-hub_fancy_heading" data-id="a645df9" data-element_type="widget" data-widget_type="hub_fancy_heading.default">
+				<div class="elementor-widget-container">
+			
+		<div id="ld-fancy-heading-616ca60ae49b1" class="ld-fancy-heading">
+			<p class="ld-fh-element lqd-highlight-underline lqd-highlight-grow-left p"> Interior design is the art and science of enhancing the interiors, sometimes including the exterior, of a space or building, to achieve a healthier and more aesthetically.</p>		</div>
+
+				</div>
+				</div>
+					</div>
+		</div>
+							</div>
+		</section>
+				<section data-section-luminosity="dark" class="elementor-section elementor-inner-section elementor-element elementor-element-bdd009d elementor-section-full_width elementor-section-height-default elementor-section-height-default" data-id="bdd009d" data-element_type="section" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+						<div class="elementor-container elementor-column-gap-default">
+					<div class="elementor-column elementor-col-100 elementor-inner-column elementor-element elementor-element-50cb58e" data-id="50cb58e" data-element_type="column">
+			<div class="elementor-widget-wrap elementor-element-populated">
+								<div class="elementor-element elementor-element-b281595 elementor-widget elementor-widget-hub_fancy_heading" data-id="b281595" data-element_type="widget" data-widget_type="hub_fancy_heading.default">
+				<div class="elementor-widget-container">
+			
+		<div id="ld-fancy-heading-616ca60ae7802" class="ld-fancy-heading">
+			<p class="ld-fh-element lqd-highlight-underline lqd-highlight-grow-left p"> Paris</p>		</div>
+
+				</div>
+				</div>
+					</div>
+		</div>
+				<div class="elementor-column elementor-col-25 elementor-inner-column elementor-element elementor-element-83699f0" data-id="83699f0" data-element_type="column">
+			<div class="elementor-widget-wrap elementor-element-populated">
+								<div class="elementor-element elementor-element-df76791 elementor-widget elementor-widget-hub_fancy_heading" data-id="df76791" data-element_type="widget" data-widget_type="hub_fancy_heading.default">
+				<div class="elementor-widget-container">
+			
+		<div id="ld-fancy-heading-616ca60ae9549" class="ld-fancy-heading">
+			<p class="ld-fh-element lqd-highlight-underline lqd-highlight-grow-left p"> 290 Maryam Springs Courbevoie, Paris</p>		</div>
+
+				</div>
+				</div>
+					</div>
+		</div>
+				<div class="elementor-column elementor-col-25 elementor-inner-column elementor-element elementor-element-d45bcaf" data-id="d45bcaf" data-element_type="column">
+			<div class="elementor-widget-wrap elementor-element-populated">
+								<div class="elementor-element elementor-element-80c3919 elementor-widget elementor-widget-hub_fancy_heading" data-id="80c3919" data-element_type="widget" data-widget_type="hub_fancy_heading.default">
+				<div class="elementor-widget-container">
+			
+		<div id="ld-fancy-heading-616ca60aeb20f" class="ld-fancy-heading">
+			<p class="ld-fh-element lqd-highlight-underline lqd-highlight-grow-left p"> +11 21 5941 295</p>		</div>
+
+				</div>
+				</div>
+					</div>
+		</div>
+							</div>
+		</section>
+				<section data-section-luminosity="dark" class="elementor-section elementor-inner-section elementor-element elementor-element-1a511eb elementor-section-full_width elementor-section-height-default elementor-section-height-default" data-id="1a511eb" data-element_type="section" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+						<div class="elementor-container elementor-column-gap-default">
+					<div class="elementor-column elementor-col-50 elementor-inner-column elementor-element elementor-element-bdaf451" data-id="bdaf451" data-element_type="column">
+			<div class="elementor-widget-wrap elementor-element-populated">
+								<div class="elementor-element elementor-element-36ab190 elementor-widget elementor-widget-ld_custom_menu" data-id="36ab190" data-element_type="widget" data-widget_type="ld_custom_menu.default">
+				<div class="elementor-widget-container">
+			
+			<div class="lqd-fancy-menu lqd-custom-menu lqd-menu-td-none">
+			
+							<ul class="reset-ul inline-nav" id="36ab190">
+				
+						<li>
+							<a href="#"> 
+
+																	<span class="link-icon left-icon icon-next-to-label"></span>	
+								
+								Fb.  
+															</a>
+													</li>
+
+						
+						<li>
+							<a href="#"> 
+
+																	<span class="link-icon left-icon icon-next-to-label"></span>	
+								
+								Tw.  
+															</a>
+													</li>
+
+						
+						<li>
+							<a href="#"> 
+
+																	<span class="link-icon left-icon icon-next-to-label"></span>	
+								
+								In.  
+															</a>
+													</li>
+
+						
+						<li>
+							<a href="#"> 
+
+																	<span class="link-icon left-icon icon-next-to-label"></span>	
+								
+								Lk.  
+															</a>
+													</li>
+
+										</ul>
+						</div>
+				</div>
+				</div>
+					</div>
+		</div>
+				<div class="elementor-column elementor-col-50 elementor-inner-column elementor-element elementor-element-46dde82" data-id="46dde82" data-element_type="column">
+			<div class="elementor-widget-wrap elementor-element-populated">
+								<div class="elementor-element elementor-element-e0594a6 elementor-widget elementor-widget-ld_custom_menu" data-id="e0594a6" data-element_type="widget" data-widget_type="ld_custom_menu.default">
+				<div class="elementor-widget-container">
+			
+			<div class="lqd-fancy-menu lqd-custom-menu right lqd-menu-td-none">
+			
+							<ul class="reset-ul inline-nav" id="e0594a6">
+				
+						<li>
+							<a href="#"> 
+
+																	<span class="link-icon left-icon icon-next-to-label"></span>	
+								
+								Company  
+															</a>
+													</li>
+
+						
+						<li>
+							<a href="#"> 
+
+																	<span class="link-icon left-icon icon-next-to-label"></span>	
+								
+								Careers  
+															</a>
+													</li>
+
+						
+						<li>
+							<a href="#"> 
+
+																	<span class="link-icon left-icon icon-next-to-label"></span>	
+								
+								Blog  
+															</a>
+													</li>
+
+										</ul>
+						</div>
+				</div>
+				</div>
+					</div>
+		</div>
+							</div>
+		</section>
+					</div>
+		</div>
+							</div>
+		</section>
+						</div>
+					</div>
+		</footer>
+		<?
 		liquid_action( 'after_footer' );
 		?>
 
