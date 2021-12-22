fce2daeef55791fc4129f61c4a6882b6a9da3db8
SeoBoSeok <ggy7bbo@gmail.com>
2021-12-23 00:08:27 +0900

    hot fix

--- category-interactive.php
+++ category-interactive.php
@@ -141,41 +141,6 @@ get_header();
 		}
 	</style>
 <div id="wrap" class="main">
-	<!-- Header S -->
-	<!-- <header id="header">
-		<div class="h_logo">
-			<a href="index.html" title="Go to Home">
-				<i class="hl_n"></i><i class="hl_e"></i><i class="hl_w"></i><i class="hl_b"></i><i class="hl_i"></i><i class="hl_r"></i><i class="hl_d"></i><i class="hl_bar"></i>
-				<h1 class="blind"></h1>
-			</a>
-		</div>
-		<hr />
-		<nav class="h_gnb">
-			<h2 class="blind">GNB</h2>
-			<i class="hg_icon"></i>
-			<button type="button" class="hg_btn"><span class="blind">Menu Button</span></button>
-			<div class="hg_wrap">
-				<ul>
-					<li>
-						<a href="index.html" class="on"><span>인덱스</span></a>
-					</li>
-					<li>
-						<a href="about/index.html"><span>공스타 소개</span></a>
-					</li>
-					<li>
-						<a href="service/index.html"><span>서비스 소개</span></a>
-					</li>
-					<li>
-						<a href="portfolio/index.html"><span>포트폴리오</span></a>
-					</li>
-					<li>
-						<a href="contact/index.html"><span>상담신청</span></a>
-					</li>
-				</ul>
-			</div>
-		</nav>
-	</header> -->
-	<!-- Header E -->
 	<hr />
 	<!-- Aside S -->
 	<aside id="aside">
@@ -185,12 +150,6 @@ get_header();
 				<li><a href="mailto:info@.co.kr"><i class="aq_icon mail"></i><span class="blind">Send to mail</span></a></li>
 			</ul>
 		</div>
-		<!-- <div class="a_customer">
-			<h2 class="ac_title">SEARCH</h2>
-			<a href="tel:0264044224" class="">
-				<svg data-v-ae75f7f2="" data-v-9760bdba="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28" class="ico_search"><g data-v-ae75f7f2="" fill="none" fill-rule="evenodd"><g data-v-ae75f7f2="" stroke-width="1.6"><g data-v-ae75f7f2="" transform="translate(-308 -16) translate(312 20)"><circle data-v-ae75f7f2="" cx="8.944" cy="8.944" r="8.944"></circle> <path data-v-ae75f7f2="" d="M14.987 14.987L21.017 21.017"></path></g></g></g></svg>
-			</a>
-		</div> -->
 	</aside>
 	<!-- Aside E -->
 	<hr />
@@ -198,40 +157,14 @@ get_header();
 	<div id="contents">
 		<div id="portfolio" class="c_position"></div>
 	</div>
-	<!-- Contents E -->
 	<hr />
 
-	<!-- 이쪽에 contents 영역 제작 부탁드립니다 -->
-	<!-- 따로 제작이 편하시면 다른 형식으로 새로 작업하셔도 됩니다^^. -->
-	
-	<!-- 참고 사이트 (헤더, 메인슬라이더, 푸터) : http://www.newbird.co.kr/index.html -->
-	<!-- 참고 사이트 (메인 컨텐츠) : https://www.jobda.im/ -->
-
-	<!-- 감사합니다 -->
-<!-- <link rel="stylesheet" href="/wp-content/uploads/elementor/css/custom-frontend-lite.min.css"> -->
 <div id="contents-2">
 	<section class="c_portfolio c_contents sub">
 			<div class="">
 				<div class="c_title">
-					<!-- <p class="ct_e">
-						<span><mark>Passion leads to design, Design leads to performance,</mark></span><br />
-						<span><mark>Performance leads to success!</mark></span>
-					</p> -->
-					<!-- <p class="ct_k">
-						<span>성공적인 프로젝트를 약속합니다!</span>
-					</p> -->
-					<!-- <h2 class="ct_m">interactive</h2> -->
 				</div>
 				<div id="page" class="c_wrap">
-					<!-- <div class="tab div5"> -->
-						<!-- <ul> -->
-							<!-- <li><a href="#!" id="a_local" onClick="chg_portfolio('local')" class="cp_a on">로컬 컨텐츠</a></li>
-							<li><a href="#!" id="a_oneday" onClick="chg_portfolio('oneday')" class="cp_a">원데이 클래스</a></li> -->
-							<!-- <li><a href="#!" id="a_shoppingmall" onClick="chg_portfolio('shoppingmall')" class="cp_a">SHOPPING MALL</a></li>
-							<li><a href="#!" id="a_app" onClick="chg_portfolio('app')" class="cp_a">APP</a></li>
-							<li><a href="#!" id="a_crm" onClick="chg_portfolio('crm')" class="cp_a">CRM/ERP</a></li> -->
-						<!-- </ul> -->
-					<!-- </div> -->
 					<div class="cp_list" id="box_local" style="display:block">
 					<div class="contents-header">
 						<div class="title-n-menu sub">
@@ -296,6 +229,19 @@ get_header();
 								</li>   -->
 							<?php endforeach; ?> 
 						</div>
+						<?php
+		// Set up paginated links.
+	    $links = paginate_links( array(
+			'type' => 'array',
+			'prev_next' => true,
+			'prev_text' => '<span aria-hidden="true">' . wp_kses_post( __( '<i class="lqd-icn-ess icon-ion-ios-arrow-back"></i>', 'hub' ) ) . '</span>',
+			'next_text' => '<span aria-hidden="true">' . wp_kses_post( __( '<i class="lqd-icn-ess icon-ion-ios-arrow-forward"></i>', 'hub' ) ) . '</span>'
+		));
+	
+		if( !empty( $links ) ) {
+	
+			printf( '<div class="blog-nav"><nav aria-label="' . esc_attr__( 'Page navigation', 'hub' ) . '"><ul class="pagination"><li>%s</li></ul></nav></div>', join( "</li>\n\t<li>", $links ) );
+		}; ?>
 					</div>
 					</div>
 				</div>
--- search.php
+++ search.php
@@ -13,34 +13,94 @@ get_header();
 	if( have_posts() ) :
 	
 	?>
-	
-	<div class="lqd-lp-row lqd-search-results-row row d-flex flex-wrap">		
-		<?php // Start the Loop.	
-	
-			while ( have_posts() ) : the_post();
-			
-			$column_classname = liquid()->layout->has_sidebar() ? 'col-xs-12' : 'col-md-4 col-sm-6';
-			
-		?>
-		<div class="lqd-lp-column <?php echo liquid_helper()->sanitize_html_classes( $column_classname ); ?>">
-		<?php get_template_part( 'templates/blog/content', 'excerpt' ); ?>
+	<style>
+		.titlebar {display: none !important;}
+		footer.main-footer {display: none !important;}
+	</style>
+	<div id="wrap" class="main">
+	<hr />
+	<!-- Aside S -->
+	<aside id="aside">
+		<div class="a_quick">
+			<ul>
+				<li><a href="#location"><i class="aq_icon location"></i><span class="blind">Location</span></a></li>
+				<li><a href="mailto:info@.co.kr"><i class="aq_icon mail"></i><span class="blind">Send to mail</span></a></li>
+			</ul>
 		</div>
-		<?php endwhile; // End of the loop. ?>
-		<?php
-		// Set up paginated links.
-	    $links = paginate_links( array(
-			'type' => 'array',
-			'prev_next' => true,
-			'prev_text' => '<span aria-hidden="true">' . wp_kses_post( __( '<i class="lqd-icn-ess icon-ion-ios-arrow-back"></i>', 'hub' ) ) . '</span>',
-			'next_text' => '<span aria-hidden="true">' . wp_kses_post( __( '<i class="lqd-icn-ess icon-ion-ios-arrow-forward"></i>', 'hub' ) ) . '</span>'
-		));
-	
-		if( !empty( $links ) ) {
-	
-			printf( '<div class="blog-nav"><nav aria-label="' . esc_attr__( 'Page navigation', 'hub' ) . '"><ul class="pagination"><li>%s</li></ul></nav></div>', join( "</li>\n\t<li>", $links ) );
-		}; ?>
-		
+	</aside>
+	<!-- Aside E -->
+	<hr />
+	<!-- Contents S -->
+	<div id="contents">
+		<div id="portfolio" class="c_position"></div>
 	</div>
+	<hr />
+
+<div id="contents-2">
+	<section class="c_portfolio c_contents sub">
+			<div class="">
+				<div class="c_title">
+				</div>
+				<div id="page" class="c_wrap">
+					<div class="cp_list" id="box_local" style="display:block">
+					<div class="contents-header">
+						<div class="title-n-menu sub">
+							<div class="title">
+								<!-- <span>HOT</span> -->
+								<!-- <span class="image"><img src="/wp-content/themes/hub/assets/img/logotitle.png"></span>
+								<span>콘텐츠</span> -->
+							</div>
+							<!-- <div class="menu sub">
+								<button class="menu-item">전체</button>
+								<button class="menu-item">라이프</button>
+								<button class="menu-item">비즈니스</button>
+							</div> -->
+						</div>
+						<div class="card-list">
+						<?php // Start the Loop.	
+	
+	while ( have_posts() ) : the_post();
+	
+	// $column_classname = liquid()->layout->has_sidebar() ? 'col-xs-12' : 'col-md-4 col-sm-6';
+	
+?>
+
+<a href="<?php the_permalink(); ?>" class="card-item type-1">
+									<div class="image" style="background-image: url('<?php echo wp_get_attachment_url( get_post_thumbnail_id(get_the_ID()) ) ?>');"></div>
+									<div class="content">
+										<div class="label">#<?php echo get_post_meta( get_the_ID(), 'post-link-url', true);?></div>
+										<div class="title">
+											<span><?php the_title(); ?></span>
+											<span><?php echo get_post_meta( get_the_ID(), 'post-quote-url', true); ?></span>
+										</div>
+										<div class="sub">2021-10-11</div>
+										<button class="dots"><span></span></button>
+									</div>
+								</a>
+
+
+<?php endwhile; // End of the loop. ?>
+<?php
+// Set up paginated links.
+	$links = paginate_links( array(
+	'type' => 'array',
+	'prev_next' => true,
+	'prev_text' => '<span aria-hidden="true">' . wp_kses_post( __( '<i class="lqd-icn-ess icon-ion-ios-arrow-back"></i>', 'hub' ) ) . '</span>',
+	'next_text' => '<span aria-hidden="true">' . wp_kses_post( __( '<i class="lqd-icn-ess icon-ion-ios-arrow-forward"></i>', 'hub' ) ) . '</span>'
+));
+
+if( !empty( $links ) ) {
+
+	printf( '<div class="blog-nav"><nav aria-label="' . esc_attr__( 'Page navigation', 'hub' ) . '"><ul class="pagination"><li>%s</li></ul></nav></div>', join( "</li>\n\t<li>", $links ) );
+}; ?>
+						</div>
+					</div>
+					</div>
+				</div>
+			</div>
+		</section>
+</div>
+
 
 	<?php else : // If no posts were found.
 
