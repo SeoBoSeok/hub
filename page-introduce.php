2a54fc83bbd42f89c18b1bc577e7240e58ca61c2
SeoBoSeok <ggy7bbo@gmail.com>
2021-12-29 11:51:24 +0900

    hot fix

--- page-introduce.php
+++ page-introduce.php
@@ -7,7 +7,6 @@
 
 get_header();
 ?>
-<!-- <link rel="stylesheet" href="/wp-content/uploads/elementor/css/custom-frontend-lite.min.css"> -->
 <style>
   .introduce_bg {
     position: absolute;
@@ -22,9 +21,6 @@ get_header();
 <div id="contents">
   <section class="c_about">
     <div class="wrap">
-      <!-- <div class="c_title">
-        <h2 class="ct_m">ABOUT US</h2>
-      </div> -->
       <style>
         .Play-Archive {
           display: inline-block;
@@ -55,41 +51,15 @@ get_header();
         Play Archive
       </span>
       <div id="page" class="c_wrap box">
-        <!-- <h3 class="title t1 bold bar s1"><mark>ABOUT US</mark></h3> -->
         <div class="title_wrapper"><h3 class="fl title bold deco s1"><mark>Startups Archive Gongju</mark></h3></div>
         <div class="ca_txt text left">
           <p class="big light s1">
           매일 많은 콘텐츠들이 만들어지고 사라집니다.<br>
 공스타는 지역의 콘텐츠를 지키(SAVE)고<br>
 도시를 새롭게 아카이브합니다.
-            <!-- 많은 <strong class="bold">914</strong>명의  -->
-            <!-- <mark class="medium marker"><mark>“뉴버드 덕분에 웃을 수 있었고, </mark></mark><mark class="medium marker"><mark>만족스러웠고, </mark></mark><mark class="medium marker"><mark>사업에 </mark></mark><mark class="medium marker"><mark>큰 </mark></mark><mark class="medium marker"><mark>도움이 </mark></mark><mark class="medium marker"><mark>되었다”</mark></mark>고 들어왔습니다.<br />
-            그리고 앞으로도 듣고 싶습니다.<br />
-            &nbsp;<br />
-            <mark class="giant thin">뉴버드의 열정과 변화가 시들지 않는 큰 원동력이기 때문입니다.</mark> -->
           </p>
         </div>
         <div class="ca_pic"></div>
-        <!-- <div class="ca_number number_point">
-          <ul>
-            <li>
-              <strong>PROJECTS</strong>
-              <p class="number">447</p>
-            </li>
-            <li>
-              <strong>CLIENTS</strong>
-              <p class="number">412</p>
-            </li>
-            <li>
-              <strong>WORKERS</strong>
-              <p><span class="number">14</span>+</p>
-            </li>
-            <li>
-              <strong>AVERAGE CAREER</strong>
-              <p><span class="number">7</span>+</p>
-            </li>
-          </ul>
-        </div> -->
         <div class="ca_overview">
           <h3 class="fl title bold deco s1"><mark>MISSION</mark></h3>
           <ul class="text middle left cb">
@@ -111,18 +81,6 @@ get_header();
 콘텐츠와 수 많은 사람들 모두 이곳에서 볼 수 있습니다.
 </p>
             </li>
-            <!-- <li>
-              <strong class="bold ck s1">SINCE</strong>
-              <p>2012년 10월</p>
-            </li>
-            <li>
-              <strong class="bold ck s1">ADDRESS</strong>
-              <p>서울특별시 마포구 양화로10길 19, 상록빌딩 2층 202호</p>
-            </li>
-            <li>
-              <strong class="bold ck s1">BUSINESS</strong>
-              <p>Web, Mobile, App, E-commerce, Solution, Ad/Marketing</p>
-            </li> -->
           </ul>
         </div>
         <div class="ca_vision">
